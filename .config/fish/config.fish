d836b41f5a3097511f06bc0ea3e02448af611b1d
Mattia Guazzaloca <me@mettz.dev>
2024-04-01 19:26:58 +0200

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,54 @@
+# Shell start
+if test -f /usr/share/autojump/autojump.fish;
+	source /usr/share/autojump/autojump.fish;
+end
+
+set TTY (tty)
+if [ "$TTY" = "/dev/tty1" ]; then
+    exec Hyprland
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Abbreviations & aliases
+if command -v eza > /dev/null
+	abbr -a l 'eza -a'
+	abbr -a ls 'eza -a'
+	abbr -a ll 'eza -al'
+else
+	abbr -a l 'ls -a'
+	abbr -a ls 'ls -a'
+	abbr -a ll 'll -a'
+end
+
+# Utility functions
+function dotfiles
+    /usr/bin/git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" $argv
+end
+
+# Path modifications
+set PATH $PATH "$HOME/.cargo/bin"
+
+# Fish prompt
+set -g fish_prompt_pwd_dir_length 3
+
+function fish_prompt
+	set_color blue
+	set hname (hostnamectl hostname)
+	if [ $hname != "rhaenyra" ]
+		echo -n $hname
+	end		
+	if [ $PWD != $HOME ]
+		set_color white
+		echo -n (prompt_pwd)
+	end
+	set_color green
+	printf '%s' (__fish_git_prompt)
+	set_color purple
+	echo -n '> '
+	set_color normal
+end
+
+set fish_greeting
